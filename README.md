5105af3b937756dfba98ac85e53ad6ac077a77c9
kandhavivekraj <kandhavivekraj@gmail.com>
2020-07-12 16:13:05 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+This repo power up's the site [kvivek.dev](https://kvivek.dev).
+
+Built using [lego](https://astronomersiva.github.io/lego/)
---
+++ lego.js
@@ -0,0 +1,21 @@
+module.exports = {
+  "name": "kvivek.dev",
+  "url": "",
+  "inlineSource": true,
+  "critical": {
+    "inline": true,
+    "dimensions": [
+      {
+        "height": 800,
+        "width": 470
+      },
+      {
+        "height": 900,
+        "width": 1200
+      }
+    ],
+    "penthouse": {
+      "timeout": 150000
+    }
+  }
+}
